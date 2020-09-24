4828f2e6e208e219e7aeb04e5a328b04816e8f28
puakehaulani <puakehaulani@gmail.com>
2020-09-24 11:52:39 -0700

    added in base of timer function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,48 @@
 //TIMER FUNCTION
-//define variable
-var timeEl = document.querySelector(".navbar-text");
-
+//define variables
+var timeEl = document.querySelector(".navbar-text"); //links to location in html
+var secondsLeft = 67;
 //define timer function
-//call timer function
+function setTime() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = "Time: " + secondsLeft;
+
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+        }
+
+    }, 1000);
+}
+//call timer function -- need to set this inside start button on welcome page
+setTime();
+
+
+//WELCOME PAGE FUNCTION
+//define variables
+var welcomeEL = document.querySelector(".welcome");
+//define function - create elements to display instructions, button below to start quiz. also link button to timer.
+function welcome() {
+    welcomeEL.textContent = "HI";
+}
+//call function at page load
+welcome();
+
 
 //QUESTIONS FUNCTION
-//define question table variables, arrays of diff question blocks with html radio/label elements on button/line.
+//define question table variablearrays of diff question blocks with html radio/label elements on button/line.
+
+// var q1 = [{question: "", answers:["A:", "B:", "C:"]}]
+// var q2 = [{question: "", answers:["A:", "B:", "C:"]}]
+// var q3 = [{question: "", answers:["A:", "B:", "C:"]}]
+// var q4 = [{question: "", answers:["A:", "B:", "C:"]}]
+// var q5 = [{question: "", answers:["A:", "B:", "C:"]}]
+
 //define table functions, called in random order, 
+// function QuizQuesiton(question, choices, correctAnswer) {
+//     this.question = question;
+//     this.choices = choices;
+//     this.correctAnswer = correctAnswer;}
 //call table functions
 
 
