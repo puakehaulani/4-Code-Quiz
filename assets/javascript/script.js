b5e9413bc611fedd972b8fdf006abf41cbaf072d
puakehaulani <puakehaulani@gmail.com>
2020-09-24 11:10:39 -0700

    pseudocoded plan for script

---
+++ assets/javascript/script.js
@@ -0,0 +1,22 @@
+//TIMER FUNCTION
+//define variable
+var timeEl = document.querySelector(".navbar-text");
+
+//define timer function
+//call timer function
+
+//QUESTIONS FUNCTION
+//define question table variables, arrays of diff question blocks with html radio/label elements on button/line.
+//define table functions, called in random order, 
+//call table functions
+
+
+//SCORE PAGE FUNCTION
+//define scorepage
+//call scorepage
+//store score in storage
+
+
+//HIGH SCORE PAGE FUNCTIONS
+//define variable
+//get storage value and log onto page
