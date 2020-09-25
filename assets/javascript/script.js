5e6e6ad6a35fffb46f0378a9b13b775fe6229ec6
puakehaulani <puakehaulani@gmail.com>
2020-09-25 15:58:55 -0700

    got question text to appear

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,7 @@
 //TIMER FUNCTION
 //define variables
 var timeEl = document.querySelector(".navbar-text"); //links to location in html
-var secondsLeft = 76;
+var secondsLeft = 75;
 //define timer function
 function setTime() {
   var timerInterval = setInterval(function () {
@@ -15,10 +15,16 @@ function setTime() {
   }, 1000);
 }
 
+function hide() {
+  var welcomeEl = document.getElementById("welcome");
+  welcomeEl.style.display = "none";
+}
 //WELCOME PAGE FUNCTION
 //listen for click, start timer
+document.getElementById("startBtn").addEventListener("click", hide);
 document.getElementById("startBtn").addEventListener("click", setTime);
 document.getElementById("startBtn").addEventListener("click", runQuestions);
+
 //load questions, hide welcome div when questions start
 
 //QUESTIONS FUNCTION
@@ -26,9 +32,10 @@ document.getElementById("startBtn").addEventListener("click", runQuestions);
 var questionsEl = document.querySelector(".question");
 quizArr = [
   {
-    questionText: "This is question one",
-    multipleChoice: ["", "", ""],
-    answer: "this is the answer",
+    questionText:
+      "______ tag is an extension to HTML that can enclose any number of JavaScript statements.",
+    multipleChoice: ["<SCRIPT>", "<BODY>", "<HEAD>"],
+    answer: "<SCRIPT>",
   },
   {
     questionText: "This is question two",
@@ -52,9 +59,9 @@ quizArr = [
   },
 ];
 
-//define table functions,
+//function to display question,
 function questionDisplay(qtn, choice) {
-  questionsEl.innerHTML = "";
+  questionsEl.innerHTML = ""; //clears earlier content so it doesnt display all questions together
   // Title
   var questionTitle = document.createElement("p");
   var node = document.createTextNode(qtn);
@@ -66,20 +73,43 @@ function questionDisplay(qtn, choice) {
   var choiceBEl = document.createElement("li");
   var choiceCEl = document.createElement("li");
   var questionBlock = document.createElement("ol");
+  //Building
+  var nodeA = document.createTextNode(choice[0]);
+  choiceAEl.appendChild(nodeA);
+  var nodeB = document.createTextNode(choice[1]);
+  choiceBEl.appendChild(nodeB);
+  var nodeC = document.createTextNode(choice[2]);
+  choiceCEl.appendChild(nodeC);
 
+  questionsEl.appendChild(questionBlock);
   questionBlock.appendChild(choiceAEl);
   questionBlock.appendChild(choiceBEl);
   questionBlock.appendChild(choiceCEl);
-  questionsEl.appendChild(questionBlock);
+}
+//function to define choices interactivity
+//create clickable spans, onClick functionality
+function handleAnswer(ans) {
+  questionsEl.onclick = function (event) {
+    let li = event.target.closest("li");
+    if (!li) return;
+    if (!questionsEl.contains(li)) return;
+
+    if ((li.textContent = ans)) {
+      //change color green, move forward
+    } else {
+      //change color red, time penalty, and move forward
+    }
+  };
 }
 
-function handleAnswer() {}
-
+//function to run questions, wraps displaying question with loop and answer functionality
 function runQuestions() {
-  //   for (var i = 0; i < quizArr.length; i++) {
-  for (var i = 0; i < 1; i++) {
+  for (var i = 0; i < quizArr.length; i++) {
+    //   for (var i = 0; i < 1; i++) {
+    // line 82 test parameter, replace with line 81 once working
     questionDisplay(quizArr[i].questionText, quizArr[i].multipleChoice);
-    handleAnswer();
+    handleAnswer(quizArr[i].answer);
+    return;
   }
 }
 
