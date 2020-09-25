a70bb2b8c213557c74d5136165acfb583d9f7e9c
puakehaulani <puakehaulani@gmail.com>
2020-09-25 09:30:43 -0700

    defined question array

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,18 +4,16 @@ var timeEl = document.querySelector(".navbar-text"); //links to location in html
 var secondsLeft = 76;
 //define timer function
 function setTime() {
-    var timerInterval = setInterval(function () {
-        secondsLeft--;
-        timeEl.textContent = "Time: " + secondsLeft;
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = "Time: " + secondsLeft;
 
-        if (secondsLeft === 0) {
-            clearInterval(timerInterval);
-            //add in game over link
-        }
-
-    }, 1000);
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      //add in game over link
+    }
+  }, 1000);
 }
-
 
 //WELCOME PAGE FUNCTION
 //listen for click, start timer
@@ -23,27 +21,46 @@ document.getElementById("startBtn").addEventListener("click", setTime);
 document.getElementById("startBtn").addEventListener("click", questionDisplay);
 //load questions, hide welcome div when questions start
 
-
 //QUESTIONS FUNCTION
-//define question table variablearrays of diff question blocks with html radio/label elements on button/line.
-let question = "Question 1 text";
-let questionEl = document.querySelector(".question");
+//define question array
 
-function questionDisplay() {
-    questionEl.textContent = question;
+quizArr = [
+  {
+    questionText: "This is question one",
+    multipleChoice: ["", "", ""],
+    answer: "this is the answer",
+  },
+  {
+    questionText: "This is question two",
+    multipleChoice: ["", "", ""],
+    answer: "this is the answer",
+  },
+  {
+    questionText: "This is question three",
+    multipleChoice: ["", "", ""],
+    answer: "this is the answer",
+  },
+  {
+    questionText: "This is question four",
+    multipleChoice: ["", "", ""],
+    answer: "this is the answer",
+  },
+  {
+    questionText: "This is question five",
+    multipleChoice: ["", "", ""],
+    answer: "this is the answer",
+  },
+];
 
-}
+function questionDisplay() {}
 
 //define table functions, called in random order
 //call table functions
-
-
 
 //SCORE PAGE FUNCTION
 //define scorepage
 //call scorepage
 //store score in storage
-
 
 //HIGH SCORE PAGE FUNCTIONS
 //define variable
